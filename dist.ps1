a889923a42e8b10e429d3290e22df4a4c737be16
tobyxdd <tobyxdd@gmail.com>
2023-03-30 20:56:30 -0700

    fix: don't include screenshot.png

--- dist.ps1
+++ dist.ps1
@@ -5,7 +5,7 @@ $tempFolder = Join-Path -Path $sourceFolder -ChildPath "build"
 New-Item -ItemType Directory -Path $tempFolder -Force | Out-Null
 
 Copy-Item -Path "$sourceFolder\*.js" -Destination $tempFolder -Force
-Copy-Item -Path "$sourceFolder\*.png" -Destination $tempFolder -Force
+Copy-Item -Path "$sourceFolder\icon-*.png" -Destination $tempFolder -Force
 Copy-Item -Path "$sourceFolder\manifest.json" -Destination $tempFolder -Force
 
 Compress-Archive -Path "$tempFolder\*" -DestinationPath $outputZip -Force
